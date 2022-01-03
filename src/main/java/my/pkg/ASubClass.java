23523998d8e852e004197254ee46c365c094f24e
Massimo Santini <santini@di.unimi.it>
2022-01-03 13:52:24 +0100

    Added UML doclet

---
+++ src/main/java/my/pkg/ASubClass.java
@@ -0,0 +1,19 @@
+package my.pkg;
+
+/**
+ * An example subclass.
+ *
+ * <p> This is just an example, it is included to illustrate the UML Javadoc capabilities.
+ */
+public class ASubClass extends Example {
+
+  /**
+   * Builds an example object.
+   *
+   * @param aField the value to remember.
+   */
+  public ASubClass(boolean aField) {
+    super(aField);
+  }
+
+}
